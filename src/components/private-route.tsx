ce5d0cb35b3bd56cbf5fdb97992b03d90a2cc221
teplostanski <igor@teplostanski.me>
2026-04-29 17:17:09 +0300

    Связыват компонент PrivateRoute со стором

--- src/components/private-route.tsx
+++ src/components/private-route.tsx
@@ -1,14 +1,16 @@
 import { Navigate } from 'react-router-dom';
 import { routesMap } from '../shared/constants';
+import { useAppSelector } from '../shared/hooks/redux';
 
 type PrivateRouteProps = {
   children: JSX.Element;
 };
 
-function PrivateRoute({ children }: PrivateRouteProps): JSX.Element {
-  const hasAccess = true;
+const PrivateRoute = ({ children }: PrivateRouteProps) => {
+  const { authorizationStatus } = useAppSelector((state) => state.authReducer);
+  const hasAccess = authorizationStatus === 'AUTH';
 
   return hasAccess ? children : <Navigate to={routesMap.login} />;
-}
+};
 
 export { PrivateRoute };
