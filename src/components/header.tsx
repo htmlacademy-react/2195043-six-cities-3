1b031fe5c79526f7d7a2af9be96cc222f5852f93
teplostanski <igor@teplostanski.me>
2026-05-01 16:05:20 +0300

    Выносит форму логина в отдельный компонент

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,39 @@
-import { Link } from 'react-router-dom';
+import cn from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
 import { routes } from '../shared/constants';
 import { HeaderUserNav } from './header-user-nav';
 
-const Header = () => (
-  <header className="header">
-    <div className="container">
-      <div className="header__wrapper">
-        <div className="header__left">
-          <Link
-            to={routes.root}
-            className="header__logo-link header__logo-link--active"
-          >
-            <img
-              className="header__logo"
-              src="img/logo.svg"
-              alt="6 cities logo"
-              width="81"
-              height="41"
-            />
-          </Link>
+const Header = () => {
+  const { pathname } = useLocation();
+
+  const isMainPage = pathname === routes.root;
+  const isLoginPage = pathname === routes.login;
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <Link
+              to={routes.root}
+              className={cn('header__logo-link', {
+                'header__logo-link--active': isMainPage,
+              })}
+            >
+              <img
+                className="header__logo"
+                src="img/logo.svg"
+                alt="6 cities logo"
+                width="81"
+                height="41"
+              />
+            </Link>
+          </div>
+          {!isLoginPage && <HeaderUserNav />}
         </div>
-        <HeaderUserNav />
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export { Header };
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,55 +1,71 @@
+import { type ChangeEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Header } from '../../components/header';
 import { routes } from '../../shared/constants';
+import { LoginData } from '../../shared/types';
+
+type FormData = LoginData;
+type Field = keyof typeof initialFormData;
+
+const initialFormData = {
+  email: '',
+  password: '',
+} as const;
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState<FormData>(initialFormData);
+
+  const handleInputChange = (
+    event: ChangeEvent<HTMLInputElement>,
+    field: Field,
+  ) => {
+    setFormData((prev) => ({ ...prev, [field]: event.target.value }));
+  };
+
+  console.log(formData);
+
+  return (
+    <form className="login__form form" action="#" method="post">
+      <div className="login__input-wrapper form__input-wrapper">
+        <label className="visually-hidden">E-mail</label>
+        <input
+          className="login__input form__input"
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          value={formData.email}
+          onChange={(event) => handleInputChange(event, 'email')}
+        />
+      </div>
+      <div className="login__input-wrapper form__input-wrapper">
+        <label className="visually-hidden">Password</label>
+        <input
+          className="login__input form__input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          value={formData.password}
+          onChange={(event) => handleInputChange(event, 'password')}
+        />
+      </div>
+      <button className="login__submit form__submit button" type="submit">
+        Sign in
+      </button>
+    </form>
+  );
+};
 
 const LoginPage = () => (
   <div className="page page--gray page--login">
-    <header className="header">
-      <div className="container">
-        <div className="header__wrapper">
-          <div className="header__left">
-            <Link className="header__logo-link" to={routes.root}>
-              <img
-                className="header__logo"
-                src="img/logo.svg"
-                alt="6 cities logo"
-                width="81"
-                height="41"
-              />
-            </Link>
-          </div>
-        </div>
-      </div>
-    </header>
+    <Header />
 
     <main className="page__main page__main--login">
       <div className="page__login-container container">
         <section className="login">
           <h1 className="login__title">Sign in</h1>
-          <form className="login__form form" action="#" method="post">
-            <div className="login__input-wrapper form__input-wrapper">
-              <label className="visually-hidden">E-mail</label>
-              <input
-                className="login__input form__input"
-                type="email"
-                name="email"
-                placeholder="Email"
-                required
-              />
-            </div>
-            <div className="login__input-wrapper form__input-wrapper">
-              <label className="visually-hidden">Password</label>
-              <input
-                className="login__input form__input"
-                type="password"
-                name="password"
-                placeholder="Password"
-                required
-              />
-            </div>
-            <button className="login__submit form__submit button" type="submit">
-              Sign in
-            </button>
-          </form>
+          <LoginForm />
         </section>
         <section className="locations locations--login locations--current">
           <div className="locations__item">
